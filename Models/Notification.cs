f4593592784bbebffa094cc05e4f978d0a5390bb
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:59:01 -0500

    RailwayBI Models Class Library Entity Notification

---
+++ Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayBI.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public string Channel { get; set; }            // "Email", "Webhook", "Slack"
+        public string Target { get; set; }             // Address or URL
+        public string Message { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool Success { get; set; }
+    }
+}
