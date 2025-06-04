a0122027328ca6c68bbf81a5970bc52a2412389f
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:55:02 -0500

    RailwayBI Models Class Library Entity Dashboard

---
+++ Models/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Dashboard
+    {
+        public Guid DashboardId { get; set; } // Unique identifier for the dashboard
+        public string Name { get; set; } // Name of the dashboard
+        
+        public Guid UserId { get; set; } // Foreign key to the user who owns the dashboard
+
+        public ICollection<Widget> Widgets { get; set; } = new List<Widget>(); // Collection of widgets on the dashboard
+    }
+}
