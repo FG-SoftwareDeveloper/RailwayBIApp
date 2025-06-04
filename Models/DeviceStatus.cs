f7d55b1a742acbdf8c31a82155b36f1651d3a799
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:56:08 -0500

    RailwayBI Models Class Library Entity DeviceStatus

---
+++ Models/DeviceStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayBI.Models
+{
+    public class DeviceStatus
+    {
+        public string DeviceId { get; set; }
+        public bool IsConnected { get; set; }
+        public DateTime LastReported { get; set; }
+        public string Note { get; set; }
+    }
+}
