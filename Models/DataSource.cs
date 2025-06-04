3b79a738b7609ded88e812b357839eb1225b22b3
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:55:36 -0500

    RailwayBI Models Class Library Entity DataSourc

---
+++ Models/DataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DataSource
+    {
+        public Guid DataSourceId { get; set; }
+        public string Name { get; set; }
+
+        public string Type { get; set; } // e.g., SQL, NoSQL, API
+
+        public string ConnectionString { get; set; } // Connection string or API endpoint
+
+        public bool IsSimulated { get; set; } // Indicates if the data source is simulated
+
+
+    }
+}
