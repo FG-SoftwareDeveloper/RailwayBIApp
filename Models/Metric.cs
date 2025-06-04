6fe106c9a0b5833ef46331615b1b88580e2165c8
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:58:31 -0500

    RailwayBI Models Class Library Entity Metric

---
+++ Models/Metric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Metric
+    {
+
+        public Guid MetricId { get; set; }
+
+        public string MetricName { get; set; } //Temperature, Speed, etc.
+
+        public string Unit { get; set; } // e.g., seconds, bytes, count
+
+        public string DataType { get; set; } // e.g., integer, float, string
+
+        public Guid DataSourceId { get; set; } // Foreign key to DataSource
+
+        public DataSource DataSource { get; set; } // Navigation property to DataSource
+
+    }
+}
