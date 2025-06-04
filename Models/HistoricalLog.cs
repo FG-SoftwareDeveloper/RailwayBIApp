8cbfd75ed8df247e529a55d52486f84cf3dd3529
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:57:38 -0500

    RailwayBI Models Class Library Entity IntegrationLog

---
+++ Models/HistoricalLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class HistoricalLog
+    {
+        public Guid LogId { get; set; } // Unique identifier for the log entry
+
+        public string LogName { get; set; }
+
+        public string LogType { get; set; } // Type of log (e.g., error, info, debug)
+
+        public string LogDescription { get; set; }
+
+        public DateTime Timestamp { get; set; } // Timestamp of the log entry
+
+        public Guid UserId { get; set; } // Foreign key to the user who created the log entry
+
+        public ICollection<DataEvent> DataEvents { get; set; } = new List<DataEvent>(); // Collection of data events associated with the log entry
+    }
+}
---
+++ Models/IntegrationLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class IntegrationLog
+    {
+        public Guid IntegrationLogId { get; set; }
+
+        public string Endpoint { get; set; } = string.Empty;
+
+        public string RequestPayload { get; set; } = string.Empty;
+
+        public string ResponsePayload { get; set; } = string.Empty;
+
+        public DateTime TimeStamp { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+    }
+}
