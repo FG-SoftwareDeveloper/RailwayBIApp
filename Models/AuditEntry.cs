794efdd8be882176a39291bdb9892f5585ceb2f3
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 11:53:17 -0500

    RailwayBI Models Class Library Entity AuditEntry

---
+++ Models/AuditEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayBI.Models
+{
+    public class AuditEntry
+    {
+        public Guid Id { get; set; }
+        public string PerformedBy { get; set; } // username or system
+        public string Action { get; set; } // e.g., "DEVICE_RENAMED", "LOGIN_SUCCESS"
+        public string Target { get; set; } // what was affected
+        public DateTime Timestamp { get; set; }
+        public string? Notes { get; set; }
+    }
+}
