189441c04c292e4e6d34a7a9bd0e103185c838fd
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 11:51:43 -0500

    RailwayBI Models Class Library Entity Alert

---
+++ Models/Alert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayBI.Models
+{
+    public class Alert
+    {
+        public Guid Id { get; set; }
+        public string DeviceId { get; set; }
+        public string RuleName { get; set; }           // Match to TriggerRule.Name
+        public string Severity { get; set; }           // "Warning", "Critical"
+        public string Message { get; set; }            // "Fuel level below 10%"
+        public DateTime Timestamp { get; set; }
+        public bool Acknowledged { get; set; }
+
+    }
+}
