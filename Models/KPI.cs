ce750a36e1f0230a72dfd8fe4df28df2df20d421
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-06-04 12:58:06 -0500

    RailwayBI Models Class Library Entity KPI

---
+++ Models/KPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayBI.Models
+{
+    public class KPI
+    {
+        public string Title { get; set; }           // "Average Speed"
+        public double Value { get; set; }           // 45.2
+        public string Unit { get; set; }            // "MPH", "hrs", "%"
+        public string Status { get; set; }          // "Normal", "Warning", "Critical"
+        public DateTime LastUpdated { get; set; }
+    }
+}
